a2e8f913845ee5bb99274f6106ff04c64c51c3e9
gitoocool <thakurisuraj38@gmail.com>
2021-05-21 00:02:58 +0545

    03-reviews project DONE

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -1,6 +1,15 @@
 // local reviews data
 const reviews = [
   {
+    id: 0,
+    name: "suraj thakuri",
+    job: "web developer",
+    img:
+      "https://scontent.fbwa1-1.fna.fbcdn.net/v/t1.6435-9/142156929_3806587102734810_4503518599689715273_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=4jRkS_Tjyw0AX_6x4vV&_nc_oc=AQktBQyaW292Ct5rXyhl9__AEM4JzP1C5hqy_0C4N-ItvOyA_2XPaY1KYC10-04lzsHBYL2BH2ei9VnRjQW1-3xb&_nc_ht=scontent.fbwa1-1.fna&oh=ddd93a0c46cc3c3a4014e06e93cfa547&oe=60D05D8B",
+    text:
+    "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
     id: 1,
     name: "susan smith",
     job: "web developer",
@@ -37,3 +46,56 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+
+let profilePic = document.getElementById('person-img')
+let author = document.getElementById('author')
+let job = document.getElementById('job')
+let info = document.getElementById('info')
+
+const nextBtn = document.querySelector('.next-btn')
+const prevBtn = document.querySelector('.prev-btn')
+const randomBtn = document.querySelector('.random-btn')
+
+const buttons = [nextBtn, prevBtn, randomBtn]
+
+buttons.forEach(btn => {
+  //console.log(btn)
+  btn.addEventListener('click', function(e){
+    //console.log(e)
+
+    console.log(e.target.classList.contains('random-btn'))
+    if(e.target.classList.contains('prev')){
+      switchAuthor(-1)
+    }
+    else if(e.target.classList.contains('next')){
+      switchAuthor(1)
+    }
+    else{
+      let random = Math.floor(Math.random() * reviews.length)
+      switchAuthor(random)
+    }
+
+  })
+})
+
+let currentAuthorId = 0;
+function displayAuthor(currentAuthorId){
+  profilePic.src =  reviews[currentAuthorId].img
+  author.innerHTML = reviews[currentAuthorId].name
+  job.innerHTML = reviews[currentAuthorId].job
+  info.innerHTML = reviews[currentAuthorId].text
+}
+
+displayAuthor(currentAuthorId);
+
+function switchAuthor(val){
+  currentAuthorId += val
+  if(currentAuthorId >= reviews.length){
+    currentAuthorId = 0
+  }
+  else if(currentAuthorId < 0){
+    currentAuthorId = reviews.length - 1
+  }
+  displayAuthor(currentAuthorId)
+}
