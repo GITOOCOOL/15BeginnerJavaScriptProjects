0ee2bf168934b15e6b07a8837b67ad6f4cabb8f6
gitoocool <thakurisuraj38@gmail.com>
2021-05-23 22:43:46 +0545

    04-navbar DONE

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,16 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const button = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+button.addEventListener('click', function(){
+    if(links.classList.contains('show-links')){
+        links.classList.remove('show-links')
+        console.log('1')
+    }
+    else{
+        links.classList.add('show-links')
+        console.log('2')
+    }
+})
